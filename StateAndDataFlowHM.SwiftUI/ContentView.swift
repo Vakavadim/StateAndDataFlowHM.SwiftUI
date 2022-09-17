08fe12487cb3268e8a14e1a8d8f823a7f3dbe84a
Vadim Gamzaev <Vadosbox@gmail.com>
2022-09-17 23:03:21 +0300

    Initial Commit

---
+++ StateAndDataFlowHM.SwiftUI/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  StateAndDataFlowHM.SwiftUI
+//
+//  Created by Вадим Гамзаев on 16.09.2022.
+//
+// Тут будет ДЗ, но пока его нет, за ночь появится) 
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StateAndDataFlowHM.SwiftUI/StateAndDataFlowHM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowHM_SwiftUIApp.swift
+//  StateAndDataFlowHM.SwiftUI
+//
+//  Created by Вадим Гамзаев on 16.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowHM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
